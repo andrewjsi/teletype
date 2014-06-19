235b685fe5f9e69a2fc2ca377e5f3c0e1cf043a6
Andras Jeszenszky <github.jsi@jss.hu>
2014-06-19 10:46:03 +0200

    retab
    tabs converted to 4 spaces

--- teletype.c
+++ teletype.c
@@ -33,14 +33,14 @@ void doshell(void);
 void resize_clients(void);
 
 static char *shell;
-static int	master;
-static int	slave;
-static int	child;
-static int	subchild;
+static int  master;
+static int  slave;
+static int  child;
+static int  subchild;
 
-static struct	termios tt;
-static struct	winsize win;
-static char	*cflg = NULL;
+static struct   termios tt;
+static struct   winsize win;
+static char *cflg = NULL;
 
 static char *progname;
 static int serialport; // by JSI
@@ -53,173 +53,173 @@ static int beeplast = 181;
 static char clear[] = {27, 91, 72, 27, 91, 74, 0};
 
 void dumpf (const char *fmt, ...) {
-	va_list ap;
-	FILE *f = fopen("dump", "a");
-	va_start(ap, fmt);
-	vfprintf(f, fmt, ap);
-	va_end(ap);
-	fclose(f);
+    va_list ap;
+    FILE *f = fopen("dump", "a");
+    va_start(ap, fmt);
+    vfprintf(f, fmt, ap);
+    va_end(ap);
+    fclose(f);
 }
 
 void sendport (char *data, int len) {
-	int i;
-	char data2[BUFSIZ]; // A BUFSIZ az stdio.h -ban van generálva és 4096 az értéke (Dellina 2.6.28)
-	stringconv(data, len, data2, BUFSIZ);
+    int i;
+    char data2[BUFSIZ]; // A BUFSIZ az stdio.h -ban van generálva és 4096 az értéke (Dellina 2.6.28)
+    stringconv(data, len, data2, BUFSIZ);
 
-	//~ memcpy(data2, data, len);
+    //~ memcpy(data2, data, len);
 
-	char *tmp;
-	char buf[8];
-	for (i = 0; i < len; i++) {
-		tmp = data2 + i;
-		snprintf(buf, 2, "%c", (int)*tmp);
-		write(serialport, buf, 1);
+    char *tmp;
+    char buf[8];
+    for (i = 0; i < len; i++) {
+        tmp = data2 + i;
+        snprintf(buf, 2, "%c", (int)*tmp);
+        write(serialport, buf, 1);
 //~ dumpf("%d (%d)\n", (int)*tmp, len);
-	}
+    }
 
-	// csipogunk, ha már rég jártunk erre :)
-	if (time(NULL) > lastwrite + beeplast) {
-		int bell = 7;
-		write(serialport, (char*)&bell, 1);
-	}
-	lastwrite = time(NULL);
+    // csipogunk, ha már rég jártunk erre :)
+    if (time(NULL) > lastwrite + beeplast) {
+        int bell = 7;
+        write(serialport, (char*)&bell, 1);
+    }
+    lastwrite = time(NULL);
 }
 
 void banner () {
-	sendport(clear, sizeof(clear));
-	int i;
-	char *msg;
+    sendport(clear, sizeof(clear));
+    int i;
+    char *msg;
 
-	for (i = 0; i < 480; i++)
-		sendport(".", 1);
-	msg = "..............                                                      ............";
-	sendport(msg, strlen(msg));
-	msg = ".............      J S S    T E L E T Y P E    T E R M I N A L       ...........";
-	sendport(msg, strlen(msg));
-	msg = "..............                                                      ............";
-	sendport(msg, strlen(msg));
-	for (i = 0; i < 480; i++)
-		sendport(".", 1);
-	sendport("\n\n", 2);
+    for (i = 0; i < 480; i++)
+        sendport(".", 1);
+    msg = "..............                                                      ............";
+    sendport(msg, strlen(msg));
+    msg = ".............      J S S    T E L E T Y P E    T E R M I N A L       ...........";
+    sendport(msg, strlen(msg));
+    msg = "..............                                                      ............";
+    sendport(msg, strlen(msg));
+    for (i = 0; i < 480; i++)
+        sendport(".", 1);
+    sendport("\n\n", 2);
 }
 
 static int getspeed (int speed) {
     switch (speed) {
-		case 300: return B300;
-		case 600: return B600;
-		case 1200: return B1200;
-		case 1800: return B1800;
-		case 2400: return B2400;
-		case 4800: return B4800;
-		case 9600: return B9600;
-		case 19200: return B19200;
-		case 38400: return B38400;
-		case 57600: return B57600;
-		case 115200: return B115200;
-		default:
-			fprintf(stderr, "Invalid speed!\n");
-			exit(255);
-	}
+        case 300: return B300;
+        case 600: return B600;
+        case 1200: return B1200;
+        case 1800: return B1800;
+        case 2400: return B2400;
+        case 4800: return B4800;
+        case 9600: return B9600;
+        case 19200: return B19200;
+        case 38400: return B38400;
+        case 57600: return B57600;
+        case 115200: return B115200;
+        default:
+            fprintf(stderr, "Invalid speed!\n");
+            exit(255);
+    }
 }
 
 static void init_serialport (char *device, int speed) {
-	serialport = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
-	if (serialport < 0) {
-		perror(device);
-		exit(-1);
-	}
+    serialport = open(device, O_RDWR | O_NOCTTY | O_NONBLOCK);
+    if (serialport < 0) {
+        perror(device);
+        exit(-1);
+    }
 
-	fcntl(serialport, F_SETFL, 0);
-	struct termios options;
-	tcgetattr(serialport, &options); // serialport aktuális beállításainak lekérdezése
-	cfsetispeed(&options, getspeed(speed)); // bejövõ baud beállítása
-	cfsetospeed(&options, getspeed(speed)); // kimenõ baud beállítása
-	options.c_cflag |= (CLOCAL | CREAD); // vevõ engedélyezése, lokális mód beállítása
-	options.c_cflag &= ~PARENB;
-	options.c_cflag &= ~CSTOPB;
-	options.c_cflag &= ~CSIZE;
-	options.c_cflag |= CS8;
-	tcsetattr(serialport, TCSANOW, &options); // beállítások mentése
+    fcntl(serialport, F_SETFL, 0);
+    struct termios options;
+    tcgetattr(serialport, &options); // serialport aktuális beállításainak lekérdezése
+    cfsetispeed(&options, getspeed(speed)); // bejövõ baud beállítása
+    cfsetospeed(&options, getspeed(speed)); // kimenõ baud beállítása
+    options.c_cflag |= (CLOCAL | CREAD); // vevõ engedélyezése, lokális mód beállítása
+    options.c_cflag &= ~PARENB;
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag &= ~CSIZE;
+    options.c_cflag |= CS8;
+    tcsetattr(serialport, TCSANOW, &options); // beállítások mentése
 }
 
 int main( int argc, char **argv) {
-	extern int optind;
+    extern int optind;
 
-	char *p;
-	progname = argv[0];
-	if ((p = strrchr(progname, '/')) != NULL)
-		progname = p+1;
+    char *p;
+    progname = argv[0];
+    if ((p = strrchr(progname, '/')) != NULL)
+        progname = p+1;
 
-	if (argc != 3 ) {
-		fprintf(stderr, "JSS TeleType Connector (c) 2010 Andrew JSI\nusage: %s <serialport> <baud>\n", progname);
-		return 255;
-	}
-
-	init_serialport(argv[1], atoi(argv[2]));
-	printf("Connected to %s, forking...\n", argv[1]);
-	banner();
-
-	shell = getenv("SHELL");
-	if (shell == NULL)
-		shell = _PATH_BSHELL;
-
-	getmaster();
-	fixtty();
-
-	(void) signal(SIGCHLD, finish);
-	child = fork();
-	if (child < 0) {
-		perror("fork");
-		fail();
-	}
-	if (child == 0) {
-		subchild = child = fork();
-		if (child < 0) {
-			perror("fork");
-			fail();
-		}
-		if (child)
-			dooutput();
-		else
- 			doshell();
-	} else {
-		(void) signal(SIGWINCH, resize);
+    if (argc != 3 ) {
+        fprintf(stderr, "JSS TeleType Connector (c) 2010 Andrew JSI\nusage: %s <serialport> <baud>\n", progname);
+        return 255;
     }
-	doinput();
 
-	return 0;
+    init_serialport(argv[1], atoi(argv[2]));
+    printf("Connected to %s, forking...\n", argv[1]);
+    banner();
+
+    shell = getenv("SHELL");
+    if (shell == NULL)
+        shell = _PATH_BSHELL;
+
+    getmaster();
+    fixtty();
+
+    (void) signal(SIGCHLD, finish);
+    child = fork();
+    if (child < 0) {
+        perror("fork");
+        fail();
+    }
+    if (child == 0) {
+        subchild = child = fork();
+        if (child < 0) {
+            perror("fork");
+            fail();
+        }
+        if (child)
+            dooutput();
+        else
+            doshell();
+    } else {
+        (void) signal(SIGWINCH, resize);
+    }
+    doinput();
+
+    return 0;
 }
 
 void doinput () {
-	register int cc;
-	char ibuf[BUFSIZ];
+    register int cc;
+    char ibuf[BUFSIZ];
 
-	while ((cc = read(0, ibuf, BUFSIZ)) > 0)
-		(void) write(master, ibuf, cc);
-	done();
+    while ((cc = read(0, ibuf, BUFSIZ)) > 0)
+        (void) write(master, ibuf, cc);
+    done();
 }
 
 void finish (int dummy) {
-	int status;
-	register int pid;
-	register int die = 0;
+    int status;
+    register int pid;
+    register int die = 0;
 
-	while ((pid = wait3(&status, WNOHANG, 0)) > 0)
-		if (pid == child)
-			die = 1;
+    while ((pid = wait3(&status, WNOHANG, 0)) > 0)
+        if (pid == child)
+            die = 1;
 
-	if (die) {
-		done();
-	}
+    if (die) {
+        done();
+    }
 }
 
 void
 resize(int dummy) {
-	/* transmit window change information to the child */
-	(void) ioctl(0, TIOCGWINSZ, (char *)&win);
-	(void) ioctl(slave, TIOCSWINSZ, (char *)&win);
+    /* transmit window change information to the child */
+    (void) ioctl(0, TIOCGWINSZ, (char *)&win);
+    (void) ioctl(slave, TIOCSWINSZ, (char *)&win);
 
-	kill(child, SIGWINCH);
+    kill(child, SIGWINCH);
 }
 
 void fatal() {
@@ -228,7 +228,7 @@ void fatal() {
 }
 
 void sig_resize_clients (int x) {
-	resize_clients();
+    resize_clients();
 }
 
 void resize_clients () {
@@ -237,7 +237,7 @@ void resize_clients () {
   int len;
 
   /* Get rows and cols from our connection to the terminal: fd 1 */
-	(void) ioctl(0, TIOCGWINSZ, (char *)&win);
+    (void) ioctl(0, TIOCGWINSZ, (char *)&win);
   rows = win.ws_row;
   cols = win.ws_col;
 
@@ -258,19 +258,19 @@ void remove_element(int index, int *array, int total) {
 }
 
 void dooutput() {
-	int cc;
-	char obuf[BUFSIZ];
+    int cc;
+    char obuf[BUFSIZ];
     int i;
 
-/*	(void) close(0); lets use it in resize*/
-	close(slave);
+/*  (void) close(0); lets use it in resize*/
+    close(slave);
     fd_set read_set;
     fd_set should_read_set;
 
     FD_ZERO(&should_read_set);
     FD_SET(master, &should_read_set);
-	signal(SIGWINCH, sig_resize_clients);
-	for (;;) {
+    signal(SIGWINCH, sig_resize_clients);
+    for (;;) {
         int max;
         read_set = should_read_set;
 
@@ -289,86 +289,86 @@ void dooutput() {
             (void) write(1, obuf, cc);
             sendport(obuf, cc); // JSI
         }
-	}
-	done();
+    }
+    done();
 }
 
 void doshell () {
-	char *shname;
-	printf("TERM is now \"vt100\"\r\n");
-	setenv("TERM", "vt100", 1);
+    char *shname;
+    printf("TERM is now \"vt100\"\r\n");
+    setenv("TERM", "vt100", 1);
 
 #if 0
-	int t;
-	t = open(_PATH_TTY, O_RDWR);
-	if (t >= 0) {
-		(void) ioctl(t, TIOCNOTTY, (char *)0);
-		(void) close(t);
-	}
+    int t;
+    t = open(_PATH_TTY, O_RDWR);
+    if (t >= 0) {
+        (void) ioctl(t, TIOCNOTTY, (char *)0);
+        (void) close(t);
+    }
 #endif
 
-	getslave();
-	(void) close(master);
-	(void) dup2(slave, 0);
-	(void) dup2(slave, 1);
-	(void) dup2(slave, 2);
-	(void) close(slave);
+    getslave();
+    (void) close(master);
+    (void) dup2(slave, 0);
+    (void) dup2(slave, 1);
+    (void) dup2(slave, 2);
+    (void) close(slave);
 
-	shname = strrchr(shell, '/');
-	if (shname)
-		shname++;
-	else
-		shname = shell;
+    shname = strrchr(shell, '/');
+    if (shname)
+        shname++;
+    else
+        shname = shell;
 
-	if (cflg)
-		execl(shell, shname, "-c", cflg, NULL);
-	else
-		execl(shell, shname, "-i", NULL);
+    if (cflg)
+        execl(shell, shname, "-c", cflg, NULL);
+    else
+        execl(shell, shname, "-i", NULL);
 
-	perror(shell);
-	fail();
+    perror(shell);
+    fail();
 }
 
 void
 fixtty() {
-	struct termios rtt;
+    struct termios rtt;
 
-	rtt = tt;
-	cfmakeraw(&rtt);
-	rtt.c_lflag &= ~ECHO;
-	(void) tcsetattr(0, TCSAFLUSH, &rtt);
+    rtt = tt;
+    cfmakeraw(&rtt);
+    rtt.c_lflag &= ~ECHO;
+    (void) tcsetattr(0, TCSAFLUSH, &rtt);
 }
 
 void
 fail() {
 
-	(void) kill(0, SIGTERM);
-	done();
+    (void) kill(0, SIGTERM);
+    done();
 }
 
 void done() {
-	if (subchild) {
-		(void) close(master);
-	} else {
-		(void) tcsetattr(0, TCSAFLUSH, &tt);
-		printf("\nLeaving terminal\n");
-		sendport(clear, sizeof(clear));
-	}
-	exit(0);
+    if (subchild) {
+        (void) close(master);
+    } else {
+        (void) tcsetattr(0, TCSAFLUSH, &tt);
+        printf("\nLeaving terminal\n");
+        sendport(clear, sizeof(clear));
+    }
+    exit(0);
 }
 
 void getmaster () {
-	tcgetattr(0, &tt);
-	ioctl(0, TIOCGWINSZ, (char *)&win);
-	if (openpty(&master, &slave, NULL, &tt, &win) < 0) {
-		fprintf(stderr, "openpty failed\n");
-		fail();
-	}
+    tcgetattr(0, &tt);
+    ioctl(0, TIOCGWINSZ, (char *)&win);
+    if (openpty(&master, &slave, NULL, &tt, &win) < 0) {
+        fprintf(stderr, "openpty failed\n");
+        fail();
+    }
 }
 
 void getslave () {
-	setsid();
-	ioctl(slave, TIOCSCTTY, 0);
+    setsid();
+    ioctl(slave, TIOCSCTTY, 0);
 }
 
 
